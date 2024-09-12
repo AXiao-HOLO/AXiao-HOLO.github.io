217d29379e82cf8d633573738ac6329383425e56
AXiao <ax117373053@163.com>
2024-09-12 20:14:36 +0800

    Create giscus.js

---
+++ giscus.js
@@ -0,0 +1,12 @@
+const giscus = {  
+  repo: "AXiao-HOLO/AXiao-HOLO.github.io", // 替换为您的 GitHub 仓库  
+  repoId: "R_kgDOMui1rw", // 在 GitHub 上找到您的仓库 ID  
+  category: "Announcements", // 分类名称  
+  categoryId: "DIC_kwDOMui1r84CiXlK", // 在 GitHub 上找到您的分类 ID  
+  mapping: "pathname", // 映射方式  
+  reactionsEnabled: true, // 启用反应  
+  emitMetadata: true, // 发送元数据  
+  inputPosition: "bottom", // 输入框位置  
+  theme: "light", // 主题  
+  lang: "zh-CN", // 语言  
+};
